9b046c77d76ec1ecbf69876d61f2c64817d6bb04
suke28 <sukesan178@icloud.com>
2021-06-05 20:09:33 +0900

    コンフリクトの解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -29,6 +29,7 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
+
     7.times do |x|
       today_plans = []
       plans.each do |plan|
